8e8885e5b31c20a4530fb9071b58ae81b3349d94
William Stephan <william.stephan@icloud.com>
2020-03-25 21:41:10 -0500

    added morgan logging

--- server.js
+++ server.js
@@ -1,59 +1,25 @@
 require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
-
-const passport = require('passport');
-const Strategy = require('passport-facebook').Strategy;
-
-// Configure the Facebook strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-passport.use(new Strategy({
-    clientID: process.env['FACEBOOK_CLIENT_ID'],
-    clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
-    callbackURL: '/return',
-    profileFields: ['id', 'displayName', 'picture.type(large)']
-  },
-  function (accessToken, refreshToken, profile, cb) {
-    const picture = `https://graph.facebook.com/${profile.id}/picture?width=200&height=200&access_token=${accessToken}`
-    //console.log(profile);
-    return cb(null, profile);
-  }));
-// Configure Passport authenticated session persistence.
-//
-// In order to restore authentication state across HTTP requests, Passport needs
-// to serialize users into and deserialize users out of the session.  In a
-// production-quality application, this would typically be as simple as
-// supplying the user ID when serializing, and querying the user record by ID
-// from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
-// and deserialized.
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
-passport.deserializeUser(function (obj, cb) {
-  cb(null, obj);
-});
 
 
-var db = require("./models");
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
 
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+const express = require("express");
+const exphbs = require("express-handlebars");
 
+const db = require("./models");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log/access.log'), { flags: 'a' })
 
 // Middleware
+
+app.use(morgan('combined', { stream: accessLogStream })) // logging
+
+
 app.use(express.urlencoded({
   extended: false
 }));
