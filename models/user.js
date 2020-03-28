48c713e793bb43fa5ea2f28157fb6b579a213819
William Stephan <william.stephan@icloud.com>
2020-03-27 19:36:32 -0500

    updated schema and seeding

--- models/user.js
+++ models/user.js
@@ -1,15 +1,57 @@
-// USER TABLE CONFIG AND SEEDING
+// USER TABLE CONFIG
 module.exports = function (sequelize, DataTypes) {
     var User = sequelize.define("User", {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                notEmpty: true
-            }
         },
-        fb_id: {
+        fbId: {
             type: DataTypes.STRING,
+        },
+        fbToken: {
+            type: DataTypes.STRING,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        zip: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        country: {
+            type: DataTypes.STRING,
+            defaultValue: "USA",
+        },
+        email: {
+            type: DataTypes.STRING,
+        },
+        phone: {
+            type: DataTypes.STRING,
+        },
+        xOwner: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        xRenter: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+        },
+        equipRatingAvg: {
+            type: DataTypes.FLOAT(7, 2),
+            defaultValue: 0,
+        },
+        renterRatingAvg: {
+            type: DataTypes.FLOAT(7, 2),
+            defaultValue: 0,
         },
         admin: {
             type: DataTypes.BOOLEAN,
@@ -27,104 +69,6 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.BOOLEAN,
             defaultValue: false,
         },
-        loc_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-        },
-        phone: {
-            type: DataTypes.STRING,
-        },
-        renter_rating_avg: {
-            type: DataTypes.FLOAT(7, 2),
-            defaultValue: 0,
-        },
-        owner_rating_avg: {
-            type: DataTypes.FLOAT(7, 2),
-            defaultValue: 0,
-        },
-        renter_transactions: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
-        owner_transactions: {
-            type: DataTypes.INTEGER,
-            defaultValue: 0,
-        },
-        renter_total_value: {
-            type: DataTypes.FLOAT(7, 2),
-            defaultValue: 0,
-        },
-        owner_total_value: {
-            type: DataTypes.FLOAT(7, 2),
-            defaultValue: 0,
-        },
     });
-    User.sync().then(() => {  // seeds DB if NODE_SEED = yes
-        if (process.env.NODE_SEED === "yes") {
-            console.log("**************** I'M HERE!")
-            User.create({
-                name: "Edward Bobby",
-                fb_id: "N/A",
-                admin: false,
-                active: true,
-                rents: true,
-                owns: true,
-                loc_id: 1,
-                email: "edward@gmail.net",
-                phone: "512-799-5555",
-                renter_rating_avg: 1.5,
-                owner_rating_avg: 2.3,
-                renter_transactions: 3,
-                owner_transactions: 2,
-                renter_total_value: 225.54,
-                owner_total_value: 0,
-            });
-            User.create({
-                name: "Bobby Edwards",
-                fb_id: "N/A",
-                admin: false,
-                active: true,
-                rents: true,
-                owns: false,
-                loc_id: 2,
-                email: "bobby@gmail.net",
-                phone: "512-997-5555",
-                renter_rating_avg: 2.1,
-                owner_rating_avg: 0,
-                renter_transactions: 3,
-                owner_transactions: 2,
-                renter_total_value: 225.54,
-                owner_total_value: 0,
-            });
-            User.create({
-                name: "Bill Stephan",
-                fb_id: "10222313904457070",
-                admin: true,
-                active: true,
-                rents: true,
-                owns: true,
-                loc_id: 3,
-                email: "me@billstephan.com",
-                phone: "512-997-8948",
-                renter_rating_avg: 0.3,
-                owner_rating_avg: 0.8,
-                renter_transactions: 3,
-                owner_transactions: 2,
-                renter_total_value: 2253.50,
-                owner_total_value: 4321.50,
-            });
-        };
-    });
- 
-    User.associate = (models) => {
-        // associations can be defined here
-        User.hasOne(models.Location, {sourceKey: 'loc_id', foreignKey: 'id' });
-    };
-
     return User;
 };
-
-
