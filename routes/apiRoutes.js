d07359900b4f3cf39c040be1f3e4177cabcfe996
William Stephan <william.stephan@icloud.com>
2020-03-28 08:11:14 -0500

    updated apiRoutes.js to fit new schema

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@ let passport = require("../config/passport");
 
 module.exports = function (app) {
 
-  // API to get Stuff
-  app.get("/api/stuff/:id?", function (req, res) {
+  // API to get Item
+  app.get("/api/item/:id?", function (req, res) {
     let id = req.params.id;
     if (id) {
-      db.Stuff.findOne({
-        where: [{id: id}],
-        include: [db.Location, db.User, db.Category]
-      }).then(function (dbStuff) {
-        res.json(dbStuff);
+      db.Item.findOne({
+        where: [{ id: id }],
+        include: [db.User, db.Category]
+      }).then(function (dbItem) {
+        res.json(dbItem);
       });
     } else {
-      db.Stuff.findAll({
-        include: [db.Location, db.User, db.Category]
-      }).then(function (dbStuff) {
-        res.json(dbStuff);
+      db.Item.findAll({
+        include: [db.User, db.Category]
+      }).then(function (dbItem) {
+        res.json(dbItem);
       });
     }
   });
@@ -26,7 +26,7 @@ module.exports = function (app) {
     let id = req.params.id;
     if (id) {
       db.User.findOne({
-        where: [{id: id}]
+        where: [{ id: id }]
       }).then(function (dbUser) {
         res.json(dbUser);
       });
@@ -42,7 +42,7 @@ module.exports = function (app) {
     let id = req.params.id;
     if (id) {
       db.Transaction.findOne({
-        where: [{id: id}]
+        where: [{ id: id }]
       }).then(function (dbTransaction) {
         res.json(dbTransaction);
       });
@@ -53,26 +53,11 @@ module.exports = function (app) {
     }
   });
 
-  app.get("/api/location/:id?", function (req, res) {
-    let id = req.params.id;
-    if (id) {
-      db.Location.findOne({
-        where: [{id: id}]
-      }).then(function (dbLocation) {
-        res.json(dbLocation);
-      });
-    } else {
-      db.Location.findAll({}).then(function (dbLocation) {
-        res.json(dbLocation);
-      });
-    }
-  });
-
   app.get("/api/category/:id?", function (req, res) {
     let id = req.params.id;
     if (id) {
       db.Category.findOne({
-        where: [{id: id}]
+        where: [{ id: id }]
       }).then(function (dbCategory) {
         res.json(dbCategory);
       });
@@ -86,7 +71,7 @@ module.exports = function (app) {
   });
 
   // Create a new example
-  app.post("/api/stuff", function (req, res) {
+  app.post("/api/item", function (req, res) {
     db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
@@ -95,7 +80,7 @@ module.exports = function (app) {
 
 
   // Delete an example by id
-  app.delete("/api/stuff/:id", function (req, res) {
+  app.delete("/api/item/:id", function (req, res) {
     db.Example.destroy({
       where: {
         id: req.params.id
@@ -108,34 +93,34 @@ module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
