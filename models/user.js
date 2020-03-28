edfd65611520bb6997277650ef2f8985b64c215f
Matt Martin <matt@incgn.to>
2020-03-27 22:11:31 -0500

    mergin master

--- models/user.js
+++ models/user.js
@@ -83,5 +83,20 @@ module.exports = function (sequelize, DataTypes) {
             defaultValue: false,
         },
     });
+    
+    User.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    // Check to see if user is an admin
+    User.prototype.isAdmin = function (id) {
+        return bcrypt.compareSync(password, this.password);
+    };
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    User.addHook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
+
     return User;
 };
