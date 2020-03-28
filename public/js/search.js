5625e6826cdf8fc9845ae21e088ff5ee575cb18f
Matt Martin <matt@incgn.to>
2020-03-28 14:16:23 -0500

    additional data on search page

--- public/js/search.js
+++ public/js/search.js
@@ -1,10 +1,24 @@
+// this is some real hacky code
 function renderStuff(item) {
-    let view = $("<div>").addClass("row no-gutters border rounded overflow-hidden flex-md-row m-5 shadow-sm h-md-250 position-relative");
+    let view = $("<div>").addClass("row row-cols-2 no-gutters border rounded overflow-hidden flex-md-row m-5 shadow-sm h-md-250 position-relative");
     let textBox = $("<div>").addClass("col p-4 d-flex flex-column position-static");
     textBox.append($("<h2>").text(item.description).addClass("mb-0"));
     textBox.append($("<p>").text("Listed by " + item.User.name).addClass("card-text mb-auto"));
-    textBox.append($("<button>").text("Rent Now!").addClass("btn btn-primary"));
-    let pictureBox = $("<div>").addClass("col-auto d-none d-lg-block");
+    if(item.User.fb_id){
+        textBox.append($("<img>").attr({
+            "src": `http://graph.facebook.com/${item.User.fb_id}/picture?type=small`
+        }));
+    }
+    textBox.append($("<h3>").text("Average Rating"));
+    textBox.append($("<h5>").text(item.equipRating));
+    textBox.append($("<h3>").text("User Rating"));
+    textBox.append($("<h5>").text(item.User.equipRatingAvg));
+    textBox.append($("<h3>").text("Daily Cost"));
+    textBox.append($("<h5>").text("$" + item.dayCost));
+    
+    textBox.append($("<a>").text("Rent Now!").addClass("btn btn-primary").attr("href", `/rent/${item.id}`));
+    
+    let pictureBox = $("<div>").addClass("col-2 col-auto d-none d-lg-block justify-content-right");
     pictureBox.append($("<img>").attr({
         "src": item.image
     }));
